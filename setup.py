ceb2cde4f0fa2ec45a4d2755f595df54111f5ad0
Sam Dornan <sdornan@gmail.com>
2012-05-23 14:43:03 -0700

    Add setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-simpleforums',
+    version='1.0',
+    description='A simple forums app for Django.',
+    author='Sam Dornan',
+    author_email='sdornan@gmail.com',
+    url='https://github.com/sdornan/django-simpleforums/',
+    packages=['django-modelutils', 'django-pure-pagination'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
