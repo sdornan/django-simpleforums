d7f156f15d7ed49c713159d84a56dd39f723981a
Sam Dornan <sdornan@gmail.com>
2012-06-12 19:25:36 -0700

    Update master

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import setup, find_packages
 
 setup(
     name='django-simpleforums',
@@ -9,7 +9,7 @@ setup(
     author='Sam Dornan',
     author_email='sdornan@gmail.com',
     url='https://github.com/sdornan/django-simpleforums/',
-    packages=['django-autoslug', 'django-pure-pagination'],
+    packages=find_packages(),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -18,5 +18,10 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Framework :: Django',
+    ],
+    install_requires=[
+        "Django >= 1.4",
+        "django-autoslug >= 1.5.0",
+        "django-pure-pagination >= 0.1"
     ],
 )
